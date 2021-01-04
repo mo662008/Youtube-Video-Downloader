40e8294eb98ad144e22256434df90d240c0089de
mo662008 <72289694+mo662008@users.noreply.github.com>
2021-01-04 18:40:24 +0200

    Update YVD.py

--- YVD.py
+++ YVD.py
@@ -15,33 +15,34 @@ print("""Please choose video resolution:
         [5] 720p
         [6] 1080p""")
 
-x = input()
-try:
-    x = int(x)
-except:
-    print("Only accept digits")
+x = input("Please enter your choice (1 : 6): ")
 
-if x == 1:
-    if video.streams.get_lowest_resolution():
-        video.streams.get_lowest_resolution().download(output_path="c:/")
-elif x == 2:
-    if video.streams.filter(res="240", progressive=True):
-        video.streams.filter(res="240", progressive=True).download(output_path="c:/")
-elif x == 3:
-    if video.streams.filter(res="360", progressive=True):
-        video.streams.filter(res="360", progressive=True).download(output_path="c:/")
-elif x == 4:
-    if video.streams.filter(res="480", progressive=True):
-        video.streams.filter(res="480", progressive=True).download(output_path="c:/")
-elif x == 5:
-    if video.streams.filter(res="720", progressive=True):
-        video.streams.filter(res="720", progressive=True).download(output_path="c:/")
-elif x == 6:
-    if video.streams.filter(res="1080", progressive=True):
-        video.streams.filter(res="1080", progressive=True).download(output_path="c:/")
+while True:
+        if 1 <= int(x) <= 6:
+                if x == 1:
+                    if video.streams.get_lowest_resolution():
+                        video.streams.get_lowest_resolution().download(output_path="c:/")
+                elif x == 2:
+                    if video.streams.filter(res="240", progressive=True):
+                        video.streams.filter(res="240", progressive=True).download(output_path="c:/")
+                elif x == 3:
+                    if video.streams.filter(res="360", progressive=True):
+                        video.streams.filter(res="360", progressive=True).download(output_path="c:/")
+                elif x == 4:
+                    if video.streams.filter(res="480", progressive=True):
+                        video.streams.filter(res="480", progressive=True).download(output_path="c:/")
+                elif x == 5:
+                    if video.streams.filter(res="720", progressive=True):
+                        video.streams.filter(res="720", progressive=True).download(output_path="c:/")
+                elif x == 6:
+                    if video.streams.filter(res="1080", progressive=True):
+                        video.streams.filter(res="1080", progressive=True).download(output_path="c:/")
 
-# Display msg when downloading is done
-def finish():
-    print("Your video is ready!")
+                # Display msg when downloading is done
+                def finish():
+                    print("Your video is ready!")
 
-video.register_on_complete_callback(finish())
+                video.register_on_complete_callback(finish())
+                break
+                
+        x = input("Please enter your choice (1 : 6): ")          
